d1f36ebb3ce69915c4eababe080503071d5a226a
Mark Anderson <mark@chef.io>
2019-04-17 10:47:06 -0700

    Bump ohai
    Signed-off-by: Mark Anderson <mark@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -8,7 +8,7 @@ override :rubygems, version: "3.0.2"
 override :bundler, version: '~> 1.17'
 override :'omnibus-ctl', version: "master"
 override :chef, version: "ma/license_acceptance_hacks"
-override :ohai, version: "v15.0.31"
+override :ohai, version: "v15.0.32"
 override :ruby, version: "2.5.5"
 override :rubygems, version: "2.7.7"
 
